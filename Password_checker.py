9c69cb4f8bc4e51c004f4ede6687d4bb18965ef1
Sheikh Kohinoor Rahman <160453453+Kohin4@users.noreply.github.com>
2026-05-04 00:36:32 +0600

    Create Password_checker.py

---
+++ Password_checker.py
@@ -0,0 +1,8 @@
+correct_password = "#amiKohin245#"
+while True:
+    password = input("Give the Passwprd : ")
+    if password == correct_password:
+        print("\n***************Welcome***************")
+        break
+    else:
+        print("\nWrong Password,Try Again")    
